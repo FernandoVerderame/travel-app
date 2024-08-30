686b0891dc2db65d4c89692e752aab9c8743af8c
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-08-30 17:48:04 +0200

    added day model

---
+++ Backend/app/Models/Day.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Model;
+
+class Day extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'trip_id',
+        'number',
+        'date',
+    ];
+
+    public function trip()
+    {
+        return $this->belongsTo(Trip::class);
+    }
+
+    protected function title(): Attribute
+    {
+        return Attribute::make(
+            set: fn(string $value) => [
+                'title' => $value,
+                'slug' => Str::slug($value)
+            ]
+        );
+    }
+}
