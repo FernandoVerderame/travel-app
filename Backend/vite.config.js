fcc2417b5eefaea4c64249baff195b6d9900de6c
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-08-31 21:00:14 +0200

    added address.js into vite config

--- Backend/vite.config.js
+++ Backend/vite.config.js
@@ -13,7 +13,8 @@ export default defineConfig({
                 'resources/js/app.js',
                 'resources/js/delete_confirmation.js',
                 'resources/js/image_preview.js',
-                'resources/js/slug.js'
+                'resources/js/slug.js',
+                'resources/js/address.js'
             ],
             refresh: true,
         }),
