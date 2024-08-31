d006bd084454b330418cd98949077cdf8bfafef1
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-08-31 20:59:15 +0200

    added stops table

---
+++ Backend/database/migrations/2024_08_31_121035_create_stops_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Models\Day;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stops', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->string('image')->nullable();
+            $table->text('foods')->nullable();
+            $table->string('address');
+            $table->decimal('latitude', 11, 8);
+            $table->decimal('longitude', 11, 8);
+            $table->tinyInteger('rating', false, true)->default(0);
+            $table->foreignIdFor(Day::class)->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('stops', function (Blueprint $table) {
+            $table->dropForeignIdFor(Day::class);
+        });
+        Schema::dropIfExists('stops');
+    }
+};
