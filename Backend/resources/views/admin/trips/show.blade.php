0d43b69ddeaa5c7205069f9c85be04e76ba1e61f
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-09-01 16:15:14 +0200

    deleted trips show buttons

--- Backend/resources/views/admin/trips/show.blade.php
+++ Backend/resources/views/admin/trips/show.blade.php
@@ -74,16 +74,7 @@
 </div>
 
 
-<div class="card-footer bg-white d-flex justify-content-between">
+<div class="d-flex justify-content-between">
   <a href="{{ route('admin.trips.index') }}" class="btn btn-secondary"><i class="fa-solid fa-arrow-rotate-left me-2"></i>Indietro</a>
-    
-  <div class="d-flex gap-2">
-      {{-- <a href="{{route('admin.trips.edit', $trip->slug)}}" class="btn btn-warning text-white">Modifica</a> --}}
-      {{-- <form action="{{ route('admin.trips.destroy', $trip->id) }}" method="POST" class="delete-form" data-bs-toggle="modal" data-bs-target="#modal" data-trip="{{ $trip->title }}">
-        @csrf
-        @method('DELETE')
-        <button type="submit" class="btn btn-danger"><i class="fa-regular fa-trash-can me-1"></i>Elimina</button>
-      </form> --}}
-  </div>
 </div>
 @endsection
